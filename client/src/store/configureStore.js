08709c40e2ef1e87590caa02c2ee55611df15260
Michael Shuff <mjshuff23@gmail.com>
2020-12-04 17:18:55 -0500

    got workouts in redux state

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from "redux-thunk";
 import authentication from './reducers/authentication';
 import exercises from './reducers/exercises';
+import workouts from './reducers/workouts';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
     authentication,
     exercises,
+    workouts,
 });
 
 const configureStore = (initialState) => {
