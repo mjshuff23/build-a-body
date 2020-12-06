70f3b3c72160d19a1424a7cdb724bbee00ac3c5e
Michael Shuff <mjshuff23@gmail.com>
2020-12-06 18:38:02 -0500

    workouts officially mapping required exercises

--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 
 function Workouts() {
     const workoutState = useSelector(state => state.workouts);
+    const exercisesState = useSelector(state => state.exercises);
     const workouts = Object.values(workoutState.list);
     return (
         <div className='workouts'>
@@ -10,8 +11,20 @@ function Workouts() {
                 workouts.map(workout => {
                     return (
                         <>
+                            <br></br>
                             <div>{ workout.title } - { workout.type }</div>
                             <div>{ workout.description }</div>
+                            <ul>
+                                { workout.WorkoutExercises.map(exercise => (
+                                    <div>{ exercisesState.list[exercise.exercise_id] ?
+                                        <>
+                                            <li>{ exercisesState.list[exercise.exercise_id].title } - { exercisesState.list[exercise.exercise_id].type }</li>
+                                            <div></div>
+                                        </>
+
+                                        : null }</div>
+                                )) }
+                            </ul>
                             <br></br>
                         </>
                     );
